c3d6e348b700e422ee717d2779738e0d3c4e3136
lemon <lemon@lemon-Lenovo-B50-70>
2021-02-26 13:00:26 +0100

    13. Refactorizo Clase Controlador y extraigo su interfaz

--- src/main/java/org/iesalandalus/programacion/biblioteca/mvc/controlador/Controlador.java
+++ src/main/java/org/iesalandalus/programacion/biblioteca/mvc/controlador/Controlador.java
@@ -5,106 +5,133 @@ import java.util.List;
 import java.util.Map;
 
 import javax.naming.OperationNotSupportedException;
+
+import org.iesalandalus.programacion.biblioteca.mvc.modelo.IFuenteDatos;
+import org.iesalandalus.programacion.biblioteca.mvc.modelo.IModelo;
 import org.iesalandalus.programacion.biblioteca.mvc.modelo.Modelo;
 import org.iesalandalus.programacion.biblioteca.mvc.modelo.dominio.*;
-import org.iesalandalus.programacion.biblioteca.mvc.vista.Vista;
+import org.iesalandalus.programacion.biblioteca.mvc.modelo.negocio.memoria.FactoriaFuenteDatosMemoria;
+import org.iesalandalus.programacion.biblioteca.mvc.vista.IVista;
+import org.iesalandalus.programacion.biblioteca.mvc.vista.texto.VistaTexto;
 
 /**
- * @author: Jonathan Simón Sánchez
- * 
- **/
+ * @author Jonathan Simón
+ * @version v2
+ *
+ */
 
-public class Controlador {
+public class Controlador implements IControlador {
 
 	// Atributos
-	private Modelo modelo;
-	private Vista vista;
+	private IModelo modelo;
+	private IVista vista;
 
 	// M.Constructor
-	public Controlador(Modelo modelo, Vista vista) {
+	public Controlador(IModelo modelo, IVista vista) {
 		if (modelo == null) {
 			throw new IllegalArgumentException("Ha habido un error, el modelo no puede ser nulo.");
 		}
 		if (vista == null) {
 			throw new IllegalArgumentException("Ha habido un error, la vista no puede ser nula.");
 		}
-		this.modelo = new Modelo();
-		this.vista = new Vista();
+		
+		IFuenteDatos iFuenteDatos = new FactoriaFuenteDatosMemoria();
+		this.modelo = new Modelo(iFuenteDatos );
+		this.vista = new VistaTexto();
 		this.vista.setControlador(this);
 	}
 
 	// Métodos
+	@Override
 	public void comenzar() {
 		vista.comenzar();
 	}
 
+	@Override
 	public void terminar() {
 		System.out.println("Muchas gracias por su tiempo.");
 	}
 
+	@Override
 	public void insertar(Alumno alumno) throws OperationNotSupportedException {
 		modelo.insertar(alumno);
 	}
 
+	@Override
 	public void insertar(Libro libro) throws OperationNotSupportedException {
 		modelo.insertar(libro);
 	}
 
+	@Override
 	public void prestar(Prestamo prestamo) throws OperationNotSupportedException {
 		modelo.prestar(prestamo);
 	}
 
+	@Override
 	public void devolver(Prestamo prestamo, LocalDate fecha) throws OperationNotSupportedException {
 		modelo.devolver(prestamo, fecha);
 	}
 
+	@Override
 	public Alumno buscar(Alumno alumno) {
 		return modelo.buscar(alumno);
 	}
 
+	@Override
 	public Libro buscar(Libro libro) {
 		return modelo.buscar(libro);
 	}
 
+	@Override
 	public Prestamo buscar(Prestamo prestamo) {
 		return modelo.buscar(prestamo);
 	}
 
+	@Override
 	public void borrar(Alumno alumno) throws OperationNotSupportedException {
 		modelo.borrar(alumno);
 	}
 
+	@Override
 	public void borrar(Libro libro) throws OperationNotSupportedException {
 		modelo.borrar(libro);
 	}
 
+	@Override
 	public void borrar(Prestamo prestamo) throws OperationNotSupportedException {
 		modelo.borrar(prestamo);
 	}
 
+	@Override
 	public List<Alumno> getAlumnos() {
 		return modelo.getAlumnos();
 	}
 
+	@Override
 	public List<Libro> getLibros() {
 		return modelo.getLibros();
 	}
 
+	@Override
 	public List<Prestamo> getPrestamos() {
 		return modelo.getPrestamos();
 	}
 
+	@Override
 	public List<Prestamo> getPrestamos(Alumno alumno) {
 		return modelo.getPrestamos(alumno);
 	}
 
+	@Override
 	public List<Prestamo> getPrestamos(Libro libro) {
 		return modelo.getPrestamos(libro);
 	}
 
+	@Override
 	public List<Prestamo> getPrestamos(LocalDate fecha) {
 		return modelo.getPrestamos(fecha);
 	}
+	@Override
 	public Map<Curso,Integer> getEstadisticaMensualPorCurso(LocalDate fecha){
 		return modelo.getEstadisticaMensualPorCurso(fecha);
 	}
