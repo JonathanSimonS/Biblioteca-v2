7751cedace1948a4f654754adabc5d972c4c9b58
lemon <lemon@lemon-Lenovo-B50-70>
2021-02-26 13:12:34 +0100

    14. Refactorizo Clase MainApp

--- src/main/java/org/iesalandalus/programacion/biblioteca/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/biblioteca/MainApp.java
@@ -1,19 +1,26 @@
 package org.iesalandalus.programacion.biblioteca;
 
 import org.iesalandalus.programacion.biblioteca.mvc.controlador.Controlador;
+import org.iesalandalus.programacion.biblioteca.mvc.controlador.IControlador;
+import org.iesalandalus.programacion.biblioteca.mvc.modelo.FactoriaFuenteDatos;
+import org.iesalandalus.programacion.biblioteca.mvc.modelo.IFuenteDatos;
+import org.iesalandalus.programacion.biblioteca.mvc.modelo.IModelo;
 import org.iesalandalus.programacion.biblioteca.mvc.modelo.Modelo;
-import org.iesalandalus.programacion.biblioteca.mvc.vista.Vista;
+import org.iesalandalus.programacion.biblioteca.mvc.vista.FactoriaVista;
+import org.iesalandalus.programacion.biblioteca.mvc.vista.IVista;
 
 /**
- * @author: Jonathan Simón Sánchez
- * 
+ * @author Jonathan Simón
+ * @version v2
+ *
  **/
 public class MainApp {
 
 	public static void main(String[] args) {
-		Modelo modelo = new Modelo();
-		Vista vista = new Vista();
-		Controlador controlador = new Controlador(modelo, vista);
+		IFuenteDatos iFuenteDatos = FactoriaFuenteDatos.MEMORIA.crear();
+		IModelo modelo = new Modelo(iFuenteDatos);
+		IVista vista = FactoriaVista.TEXTO.crear();
+		IControlador controlador = new Controlador(modelo, vista);
 		controlador.comenzar();
 	}
 }
